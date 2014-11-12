711dfbb97ca3f90ecc79c4b5d1f8e73abed9d2e7
Sherif <ucapsib@ucl.ac.uk>
2014-11-11 19:31:10 -0800

    Speed match was just added, next is moving according to velocity

--- boids2.py
+++ boids2.py
@@ -1,77 +1,68 @@
 __author__ = 'Sherif'
-
+import random
+from numpy import array
 class boids (type):
     def __init__(self, boids_x, boids_y, boid_x_velocities, boid_y_velocities):
-        from random import random
         for x in range (50):
-        self.boids_x=random.uniform(-450,50.0)
-        self.boids_y=random.uniform(300.0,600.0)
-        self.boid_x_velocities=random.uniform(-20.0,20.0)
-        self.boid_x_velocities=boid_x_velocities
-        boids = [boids(type) for type in boids_x, boids_y, boid_x_velocities, boid_y_velocities
+         self.boids_x=random.uniform(-450,50.0)
+         self.boids_y=random.uniform(300.0,600.0)
+         self.boid_x_velocities=random.uniform(-20.0,20.0)
+         self.boid_x_velocities=boid_x_velocities
+        boids = [boids(type) for type in boids_x, boids_y, boid_x_velocities, boid_y_velocities]
 
     def update_boids(boids):
         xs,ys,xvs,yvs=boids
-
-        #Fly towards the middle
-        # breaking down the function into
-    def calculate (self, A, B):
+        # Fly towards
+    def velocity (self, xvs, yvs):
         for i in range(len(xs)):
-		    j in range(len(xs)):
-            self.A=(xs[j]-xs[i])*0.01/len(xs)
-            self.B=(ys[j]-ys[i])*0.01/len(xs))
-    def calculate2 (self, xvs, yvs):
+            for j in range(len(xs)):
+                self.xvs, self.yvs= xvs, yvs
+    def change_velcoity (self, A, B):
         for i in range(len(xs)):
-		    j in range(len(xs)):
-        return calculate(xvs[i]+=self.A, yvs[i]+=self.B)
+            for j in range(len(xs)):
+                A=(xs[j]-xs[i])*0.01/len(xs)
+                B=(ys[j]-ys[i])*0.01/len(xs)
+            return change_velcoity(self.xvs[i]+A, yvs[i]+B)
 
 
-
-
-            A=
-
-
-            return C=(xs[j]-xs[i])*0.01/len(xs)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        # Moving away
+    def moving_away (self, xs, xy):
+            self.xs=xs
+            self.xy=xy
+    def distance(self, xa, ya, xb, yb):
+        return (xb-xa)**2 + (yb-ya)**2
+    def compute_sum (self, xvs, yvs):
+        for i in range(len(self.xs)):
+            for j in range(len(self.xs)):
+                if (self.distance(self.xs[i],self.ys[i], self.xs[j], self.ys[j])) < 100:
+                    self.xvs[i]+= self.xs[i]-self.xs[j]
+                    self.yvs[i]+= (self.ys[i]-self.ys[j])
+# Speed match
+    def speed_match (self, xvs, yvs):
+        for i in range(len(self.xs)):
+            for j in range(len(self.xs)):
+                if (self.distance(self.xs[i],self.ys[i], self.xs[j], self.ys[j])) < 10000:
+                    self.xvs[i]+=(self.xvs[j]-self.xvs[i])*0.125/len(self.xs)
 
 
 
 
 
 
+# figure=plt.figure()
+# axes=plt.axes(xlim=(-500,1500), ylim=(-500,1500))
+# scatter=axes.scatter(boids[0],boids[1])
 
-figure=plt.figure()
-axes=plt.axes(xlim=(-500,1500), ylim=(-500,1500))
-scatter=axes.scatter(boids[0],boids[1])
-
-def animate(frame):
-   update_boids(boids)
-   scatter.set_offsets(zip(boids[0],boids[1]))
+# def animate(frame):
+#   update_boids(boids)
+#   scatter.set_offsets(zip(boids[0],boids[1]))
 
 
-anim = animation.FuncAnimation(figure, animate,
-                               frames=50, interval=50)
+# anim = animation.FuncAnimation(figure, animate,
+#                               frames=50, interval=50)
 
-if __name__ == "__main__":
-    plt.show()
+# if __name__ == "__main__":
+#    plt.show()
 
 
 
