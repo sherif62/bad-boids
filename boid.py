b55cc6eef28e2490a405580f4458e719b1c10835
Sherif <ucapsib@ucl.ac.uk>
2014-11-11 21:45:29 -0800

    The flying conditions

--- boid.py
+++ boid.py
@@ -6,6 +6,18 @@ class boid (object):
     def __init__(self, px, py, vx, vy):
         config = yaml.load(open(os.path.join(os.path.dirname(__file__), 'initial_config.yml')))
         self.ran.boid= config["ran_boids"]
+        self.fly_mid=config["fly_mid"]
+        self.fly_away= config["fly_away"]
+        self.speed_match= config ["speed_match"]
+        self.speed_match2= config["speed_match2"]
+
+        ## For each boid
+        self.px=px
+        self.py=py
+        self.vx=vx
+        self.vy=vy
+
+
 
 
 
